797b8c47811e6b10d2ea3a4641783f2051087002
Leedonghun03 <hanrcboy2@naver.com>
2022-05-19 22:29:00 +0900

    러시안 룰렛 게임
    과제 러시안 룰렛 제작 완료

---
+++ homework/homework.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace  std;
+
+int main()
+{
+    //과제 1
+    /*int i = 0, m;
+    
+    cout << "정수를 입력해주세요: ";
+    cin >> i;
+
+    for (int n = 1; n <= i; n++)
+    {
+        m = n * 3;
+        cout << m << " ";
+    }
+    cout << endl;
+
+    return 0;*/
+
+    //과제 2
+    /*short i = 0;
+
+    while (i > -1)
+    {
+        cout << i << endl;
+        i++;
+    }
+    cout << "사용 가능량을 넘었습니다." << endl;*/
+}
